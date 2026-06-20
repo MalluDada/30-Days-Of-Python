69f6dc93e661a8beea5682e9598a79e4e61a765d
Malhar Sagara <malharsagara123@gmail.com>
2026-06-20 09:46:05 +0530

    Day 3 - Operators and Booleans completed

---
+++ Operators.py
@@ -0,0 +1,25 @@
+""""
+base= float(input("Enter base length "))
+height= float(input("Enter height length "))
+Area_of_triangle= 1/2*base*height
+
+print('Area Of triangle is ',Area_of_triangle )
+"""
+"""
+x_1= float(input("Enter x1  "))
+y_1= float(input("Enter y1  "))
+x_2= float(input("Enter x2  "))
+y_2= float(input("Enter y2  "))
+
+Slope= float((y_2 - y_1)/(x_2 - x_1))
+Euclidean_distance= float((x_2 - x_1)**2 +  (y_2 - y_1)**2)**(1/2)
+print('Slope is ', Slope)
+print('Euclidean Distance is  ',Euclidean_distance)
+"""
+"""
+print('jargon in full of jargon ',  'jargon' in 'full of jargon')
+"""
+
+hours= int(input("Enter hours: "))
+rate_per_hour= float(input("Enter rate per hour: "))
+print('Total bill is:  ', hours*rate_per_hour)
