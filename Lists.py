c69415adc07c68d8f622f2cac1cb143e5e8c9ef0
Malhar Sagara <malharsagara123@gmail.com>
2026-06-23 22:39:15 +0530

    Day 5 - Lists COmpleted

---
+++ Lists.py
@@ -0,0 +1,307 @@
+'''
+empty_list= list()
+print(empty_list)
+
+fruits = ['Banana', 'Papaya', 'Mango', 'Chickoo', 'Pineapple']
+print(fruits)
+print(len(fruits))
+
+first_fruit = fruits[0]
+middle_fruit = fruits[2]
+last_fruit = fruits[4]
+
+print(first_fruit)
+print(middle_fruit)
+print(last_fruit)
+'''
+'''
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+it_companies[5] = 'Netflix'
+
+
+print(it_companies)
+it_companies.append('Oracle')
+
+print(it_companies)
+
+it_companies.insert(4, 'OpenAI')
+print(it_companies)
+
+it_companies[0]=it_companies[0].upper()
+print(it_companies)
+
+print('#;'.join(it_companies))
+
+is_there= 'IBM' in it_companies
+
+print(is_there)
+
+reverse_companies = sorted(it_companies, reverse=True)
+print(reverse_companies)
+
+
+print(it_companies[0:3])
+print(it_companies[6:])
+print(it_companies[3:5])
+
+it_companies.pop(0)
+print(it_companies)
+'''
+'''
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+joined_list = front_end + back_end
+print(joined_list)
+
+full_stack = joined_list.copy()
+
+print(full_stack)
+
+full_stack.insert(6, 'Python')
+full_stack.isert(7, 'SQL')
+
+print(full_stack)
+'''
+'''
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+ages.sort()
+print(ages)
+
+print('Maximum: ', max(ages))
+print('Minimum: ', min(ages))
+
+median = (ages[4] + ages[5])/2
+
+print('Median age: ', median)
+
+sum_ages = sum(ages)
+
+average_age = sum_ages/2 
+print(average_age)
+
+range_age = max(ages) - min(ages)
+print('Range: ', range_age)
+
+absolute_min = abs(min(ages) - average_age)
+absolute_max = abs(max(ages) - average_age)
+
+print('Absolute Max: ', absolute_max)
+print('Absolute Min: ', absolute_min)
+
+print(absolute_max < absolute_min)
+'''
+'''
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+]
+
+print(len(countries))
+
+print(countries[len(countries)//2])
+print(countries[0:98])
+
+print(countries[98:196])
+'''
+countries= ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+
+ch, ru, us, *scandic = countries
+
+print(ch)
+print(ru)
+print(us)
+print(scandic)
